eebb451662d9a407ef24255ebc7c1d259c2d6a67
adel ashraf <adel4ashraf@outlook.com>
2023-03-14 13:27:30 +0200

    Initiated the four main models

---
+++ app/Http/Controllers/BarberController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barber;
+use App\Http\Requests\StoreBarberRequest;
+use App\Http\Requests\UpdateBarberRequest;
+
+class BarberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBarberRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Barber $barber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Barber $barber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBarberRequest $request, Barber $barber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Barber $barber)
+    {
+        //
+    }
+}
