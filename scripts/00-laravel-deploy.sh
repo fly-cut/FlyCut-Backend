a108d6d4744fe68da2b65c2b12625bbe099eaf90
adel ashraf <adel4ashraf@outlook.com>
2023-05-11 00:48:47 +0300

    edit

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -13,5 +13,4 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
-php artisan vendor:publish --provider "L5Swagger\L5SwaggerServiceProvider"
 php artisan l5-swagger:generate
