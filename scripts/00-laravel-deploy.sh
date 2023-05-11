1a3a692a787ce9fae5ffaad07ae261f6f52aa35c
adel ashraf <adel4ashraf@outlook.com>
2023-05-11 10:00:13 +0300

    edit

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 echo "Running composer"
 composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
 composer global require "darkaonline/l5-swagger"
+composer install --no-dev --working-dir=/var/www/html
+
 
 echo "Caching config..."
 php artisan config:cache
