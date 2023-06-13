11596f8f8970951e59921f0c25020761510b009e
unknown <adel4ashraf@outlook.com>
2023-06-13 14:26:31 +0300

    rewrote the haircut controller

--- app/Http/Controllers/HairCutController.php
+++ app/Http/Controllers/HairCutController.php
@@ -10,28 +10,37 @@ class HairCutController extends Controller
 {
     public function getAllHaircuts()
     {
-        $searchTerm = 'haircut';
-
-        $haircuts_service = Service::whereRaw('LOWER(REPLACE(name, " ", "")) LIKE ?', ['%' . strtolower(str_replace(' ', '', $searchTerm)) . '%'])
-            ->first();
-        $haircut_service_id = $haircuts_service->id;
-        return $haircut_service_id;
-        //search for all variations of haircut
-        $haircut_variations = Variation::where('service_id', $haircut_service_id)->get();
+        $service = Service::where('name', 'haircut')->get()->first();
+        if (!$service) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No service found'
+            ]);
+        }
+        $haircuts = Variation::where('service_id', $service->id)->get();
+        if ($haircuts->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No haircuts found'
+            ]);
+        }
         return response()->json([
-            'message' => 'All haircuts',
-            'data' => $haircut_variations
-        ], 200);
+            'success' => true,
+            'data' => $haircuts
+        ]);
     }
     public function search(Request $request)
     {
-        //search for variations of haircut
-        $searchTerm = $request->input('hairCutName');
-        $haircuts_service_id = Service::whereRaw('LOWER(REPLACE(name, " ", "")) LIKE ?', ['%' . strtolower(str_replace(' ', '', 'haircut')) . '%'])
-            ->first()->id;
-        $haircuts = Variation::whereRaw('LOWER(REPLACE(name, " ", "")) LIKE ?', ['%' . strtolower(str_replace(' ', '', $searchTerm)) . '%'])
-            ->where('service_id', $haircuts_service_id)
-            ->get();
-        return $haircuts;
+        $haircut = Variation::where('name', 'like', '%' . $request->name . '%')->get();
+        if ($haircut->isEmpty()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'No haircut found'
+            ]);
+        }
+        return response()->json([
+            'success' => true,
+            'data' => $haircut
+        ]);
     }
 }
