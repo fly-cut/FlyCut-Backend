862c8933168cf9ee86f4f618647a17ba97e76539
unknown <adel4ashraf@outlook.com>
2023-06-24 16:30:05 +0300

    edited reservations

--- database/migrations/2023_06_24_131209_update_reservations_table.php
+++ database/migrations/2023_06_24_131209_update_reservations_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('reservations', function (Blueprint $table) {
-            $table->string('date');
+            $table->string('date')->nullable();
         });
     }
 
