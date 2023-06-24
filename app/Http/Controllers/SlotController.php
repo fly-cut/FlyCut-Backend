8caddd4821d525822f7267618b5280b1352419c3
kareemgalall <60933161+kareemgalall@users.noreply.github.com>
2023-06-24 19:02:10 +0300

    added barbershop_id in getting reserved slots

--- app/Http/Controllers/SlotController.php
+++ app/Http/Controllers/SlotController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Slot;
+use App\Models\Barber;
 use Illuminate\Http\Request;
 
 class SlotController extends Controller
@@ -13,10 +14,20 @@ class SlotController extends Controller
         $request->validate([
             'day' => 'required|date_format:Y-m-d',
             'barber_id' => 'required|exists:barbers,id',
+            'barbershop_id' => 'required|exists:barbershops,id',
         ]);
-        $slots = Slot::where('start_time', 'LIKE', $request->day.'%')
+        $barber = Barber::findOrFail($request->barber_id);
+        $barbershop = $barber->barbershop;
+        //check that barber really works in the barbershop
+        if ($barbershop->id != $request->barbershop_id) {
+            return response()->json([
+                'message' => 'Barber does not work in this barbershop',
+            ], 403);
+        }
+        $slots = Slot::where('start_time', 'LIKE', $request->day . '%')
             ->where('barber_id', $request->barber_id)
             ->get();
+        //check that barber really works in the barbershop
 
         return response()->json($slots);
     }
