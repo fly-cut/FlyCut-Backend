03e530b7791e5fad520ffc36f059091f65c34383
unknown <adel4ashraf@outlook.com>
2023-06-25 14:55:44 +0300

    migrate fresh1

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,7 +11,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:fresh
+php artisan migrate:refresh
 
 echo "installing passport..."
 php artisan passport:install
