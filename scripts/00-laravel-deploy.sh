14fd9a8b5da974ffd4a3b339882848195a868379
unknown <adel4ashraf@outlook.com>
2023-06-25 15:27:11 +0300

    add migrate install

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -11,8 +11,7 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate:install
-php artisan migrate --force
+php artisan migrate:fresh --force
 
 echo "installing passport..."
 php artisan passport:install
