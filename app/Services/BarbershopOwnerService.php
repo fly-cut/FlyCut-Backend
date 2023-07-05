4bd75c8a028c740ff032538c261d73eb498039cf
kareemgalall <60933161+kareemgalall@users.noreply.github.com>
2023-07-05 19:30:44 +0300

    updated barbershop owner service

--- app/Services/BarbershopOwnerService.php
+++ app/Services/BarbershopOwnerService.php
@@ -2,15 +2,20 @@
 
 namespace App\Services;
 
+use App\Repositories\BarbershopOwnerRepository;
 use Illuminate\Support\Facades\Hash;
 
 class BarbershopOwnerService
 {
-    public function changePassword($request)
+    private $barbershopOwnerRepository;
+
+    public function __construct(BarbershopOwnerRepository $barbershopOwnerRepository)
     {
-        $user = $request->user();
-        $current_password = $request->current_password;
-        $new_password = $request->new_password;
+        $this->barbershopOwnerRepository = $barbershopOwnerRepository;
+    }
+
+    public function changePassword($user, $current_password, $new_password)
+    {
         if (!Hash::check($current_password, $user->password)) {
             $message = [
                 'message' => 'Password isn\'t correct',
@@ -18,7 +23,9 @@ class BarbershopOwnerService
 
             return response($message, 422);
         }
-        $user->update(['password' => Hash::make($new_password)]);
+
+        $this->barbershopOwnerRepository->updatePasswordByEmail($user->email, $new_password);
+
         $message = [
             'message' => 'Password changed successfully',
         ];
@@ -26,18 +33,18 @@ class BarbershopOwnerService
         return response($message, 200);
     }
 
-    public function updateProfile($request)
+    public function updateProfile($user, $formData)
     {
-        $user = $request->user();
-        $formData = $request->all();
-        if ($request->file('image')) {
-            $image = $request->file('image');
+        if (isset($formData['image'])) {
+            $image = $formData['image'];
             $image_name = time() . '.' . $image->getClientOriginalExtension();
 
             $image->move(public_path('images/'), $image_name);
             $formData['image'] = $image_name;
         }
-        $user->update($formData);
+
+        $this->barbershopOwnerRepository->update($user, $formData);
+
         $message = [
             'message' => 'Profile updated successfully',
             'barbershopOwner' => $user,
@@ -45,14 +52,16 @@ class BarbershopOwnerService
 
         return response($message, 200);
     }
-    public function assignToken($request)
+
+    public function assignToken($owner, $token)
     {
-        $owner = $request->user();
-        $owner->token = $request->token;
+        $owner->token = $token;
         $owner->save();
+
         $message = [
             'message' => 'Token assigned successfully',
         ];
+
         return response($message, 200);
     }
 }
